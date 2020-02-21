5d394ca7b5b0fb0c0d2b55e8980aa8c03bbc135a
Diego Uribe <diego.uribe@berkeley.edu>
2020-02-21 05:07:25 +0000

    implemented read, seek, tell, close

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -30,6 +30,20 @@ get_file_from_fd(int fd) {
 }
 
 void
+remove_file(int fd) {
+  struct list l = thread_current()->fd_map;
+  thread_fd_t *w;
+  for (struct list_elem *e = list_begin(&l); e->next != NULL; e = e->next) {
+    w = list_entry(e, thread_fd_t, elem);
+    if (w->fd == fd) {
+      list_remove(e);
+      return;
+    }
+  }
+  return NULL;
+}
+
+void
 syscall_init (void)
 {
   lock_init(&global_file_lock);
@@ -52,24 +66,44 @@ void file_operation_handler(struct intr_frame *f) {
       break;               /* Obtain a file's size. */
 
     // BEN AND DIEGO
-    case SYS_READ:
-      break;                   /* Read from a file. */
-    case SYS_WRITE: {
-        int size = args[1];
+    case SYS_READ: {
+        int size = args[3];
         void *buffer = (void *)args[2];
-        int fd = args[3];
+        int fd = args[1];
+        struct file *file_ = get_file_from_fd(fd);
+        int read_bytes = file_read(file_, buffer, size);
+        f->eax = read_bytes;
+        break;
+    }                  /* Read from a file. */
+    case SYS_WRITE: {
+        int size = args[3];
+        void *buffer = (void *)args[2];
+        int fd = args[1];
         struct file *file_ = get_file_from_fd(fd);
         int written_bytes = file_write(file_, buffer, size);
         f->eax = written_bytes;
         break;
       }                  /* Write to a file. */
-    case SYS_SEEK:
-      break;                   /* Change position in a file. */
-    case SYS_TELL:
-      break;                   /* Report current position in a file. */
-    case SYS_CLOSE:
-      break;
- 
+    case SYS_SEEK: {
+        int new_pos = args[2];
+        int fd = args[1];
+        struct file *file_ = get_file_from_fd(fd);
+        file_seek(file_, new_pos);
+        break;            /* Change position in a file. */
+    }                 
+    case SYS_TELL: {
+          int fd = args[1];
+          int pos = file_tell(fd);
+          f->eax = pos;
+          break;   
+    }                /* Report current position in a file. */
+    case SYS_CLOSE: {
+          int fd = args[1];
+          struct file *file_ = get_file_from_fd(fd);
+          file_close(file_);
+          remove_file(fd);
+          break;
+    } 
   }
   lock_release(&global_file_lock);
   thread_exit ();
